f68727f37ee588b9611106d5414033df90ae8d31
Andy Warmack <awarmack@gmail.com>
2024-02-19 03:25:15 -0600

    refactored some code and added testing functions

--- crunchy.py
+++ crunchy.py
@@ -8,7 +8,7 @@
  *
 '''
 
-import fileinput, re, sys
+import fileinput, re, sys, traceback
 from os.path import exists
 
 ####################
@@ -22,6 +22,14 @@ output_ = True
 goto_ = ''
 max_read_depth_ = 5
 read_path_ = ''
+
+testing_ = False
+test_filename_ = None
+test_f_ = None
+test_pause_ = False
+test_verbose_ = False
+test_pass_ = 0
+test_fail_ = 0
 
 fulbal_ = 0.0
 clrbal_ = 0.0
@@ -42,9 +50,11 @@ map_ = None
 
 def show_info(should_show = False):
     if should_show or len(sys.argv) == 1:
-        print("""
+        printLine("""
 Crunchy Report Generator aka Crunch Really Useful Numbers Coded Hackishly
 """)
+
+################################################################################
 
 def skipLine(line):
     global comment_mode_, goto_
@@ -63,6 +73,8 @@ def skipLine(line):
     if goto_: return True
     return False
 
+####################
+
 def makeHeaders():
     global elements_, width_, justify_
     width_ = [None] * len(elements_)
@@ -79,6 +91,8 @@ def makeHeaders():
         elements_[i] = re.sub('[\<\|\>]$', '', element)
     return elements_
 
+####################
+
 def getElements(line):
     delim = '~'
     line = re.sub('^\s*', '', line)
@@ -89,20 +103,84 @@ def getElements(line):
             elements_[i] = ' '
     return elements_
 
+################################################################################
+
 def ljustify(str, width):
     return str.ljust(width)[:width]
+
+####################
 
 def cjustify(str, width):
     return str.center(width)[:width]
 
+####################
+
 def rjustify(str, width):
     return str.rjust(width)[:width]
 
+################################################################################
+
 def infoMessage(message):
-    if infomsg_ and output_: print('<i> ' + message)
+    if infomsg_ and output_: printLine('<i> ' + message)
+
+####################
 
 def errorMessage(message):
-    print('<E> ' + message, file=sys.stderr)
+    printLine('<E> ' + message, sys.stderr)
+
+################################################################################
+
+def testMessage(message, verbose = False):
+    global test_verbose_
+    if test_verbose_ or verbose: print('<T> ' + message)
+
+####################
+
+def testStop(verbose = False):
+    global testing_, test_filename_, test_f_, test_pause_, test_pass_, test_fail_
+    test_filename_ = ''
+    testing_ = False
+    test_pause_ = False
+    if test_f_ != None: test_f_.close()
+    testMessage('Test stopped.', verbose)
+    total = test_pass_ + test_fail_
+    s = 's' if total != 1 else ''
+    testMessage(str(test_pass_ + test_fail_) + ' line' + s + ' tested: ' + str(test_pass_) + ' passed, ' + str(test_fail_) + ' failed', True)
+
+################################################################################
+
+def printLine(line = '', stdio=sys.stdout):
+    global testing_, test_filename_, test_f_, test_pass_, test_fail_
+    if testing_:
+        try:
+            if test_f_ == None:
+                if exists(test_filename_):
+                    test_f_ = open(test_filename_, 'r')
+                else:
+                    testMessage('File \'' + test_filename_ + '\' does not exist; stopping test.', True)
+                    testStop(True)
+            if test_f_ != None:
+                line = re.sub('\n', '', line)
+                test_line = test_f_.readline()
+                if test_line != '':
+                    test_line = re.sub('\n', '', test_line)
+                    if line == test_line:
+                        testMessage('Passed: ' + line)
+                        test_pass_ += 1
+                    else:
+                        testMessage('Expected: ' + test_line, True)
+                        testMessage('Received: ' + line, True)
+                        test_fail_ += 1
+                else:
+                    testMessage('Unexpected EOF reached; stopping test.', True)
+                    testStop(True)
+        except:
+            testMessage('Unexpected error: ' + line, True)
+            traceback.print_exc()
+    if not testing_:
+        print(line, file=stdio)
+
+################################################################################
 
 def debugList(listname, list = []):
     if listname in globals():
@@ -112,56 +190,77 @@ def debugList(listname, list = []):
         print('`' + str(i), end='')
     print('')
 
+################################################################################
+
 def parseDirective(line):
     global running_, infomsg_, output_, goto_, max_read_depth_, read_path_
+    global testing_, test_filename_, test_pause, test_verbose_
     global fulbal_, clrbal_, catfield_, clrfield_, incfield_, decfield_, map_
     argtrim = ''
     arg = None
-    m = re.search('^\s*\S(\S*)\s(\s*)(\S.*\S)(\s*)$', line)
+    m = re.search('^\s*\S(\S*)\s(.*)$', line)
     if m:
-        cmd = m.group(1)
-        arg = m.group(2) + m.group(3) + m.group(4)
-        argtrim = m.group(3)
+        arg = m.group(2)
+        argtrim = arg.strip()
     else:
         m = re.search('^\s*\S(\S*)$', line)
-        if m:
-            cmd = m.group(1)
-        else:
-            m = re.search('^\s*\S(\S*)\s(\S*)\s(\S*)\s*$', line)
-            if m:
-                cmd = m.group(1)
-                arg1 = m.group(2)
-                arg2 = m.group(3)
+        arg = None
+        argtrim = None
+    cmd = m.group(1)
+
+    ####################
+
     if cmd == 'cd':
         read_path_ = argtrim
         if read_path_:
             infoMessage('Setting current working directory to \'' + read_path_ + '\'.')
         else:
             infoMessage('Resetting current working directory.')
+
+    ####################
+
     elif cmd == 'goto':
         goto_ = argtrim
         infoMessage('Skipping to \'' + goto_ + '\'.')
+
+    ####################
+
     elif cmd == 'header': pass
+
+    ####################
+
     elif cmd == 'init':
         fulbal_ = float(argtrim)
         clrbal_ = fulbal_
         infoMessage('Initializing balance to {:.2f}'.format(fulbal_) + '.')
+
+    ####################
+
     elif cmd == 'map':
         premap = getElements(argtrim)
         map_ = [None] * len(premap)
         for m, element in enumerate(premap):
             map_[int(premap[m]) - 1] = m + 1
         infoMessage('Fields were remapped.')
+
+    ####################
+
     elif cmd == 'output' and arg == 'on':
         output_ = True
         infoMessage('Output mode is on.')
     elif cmd == 'output' and arg == 'off':
         infoMessage('Output mode is off.')
         output_ = False
+
+    ####################
+
     elif cmd == 'print' and output_:
-        print(arg) if arg is not None else print()
+        printLine(arg) if arg is not None else printLine()
     elif cmd == 'print' and not output_:
         pass
+
+    ####################
+
     elif cmd == 'read':
         read_source = argtrim
         if read_path_:
@@ -176,10 +275,13 @@ def parseDirective(line):
                 f.close()
                 infoMessage('Finished reading file ' + read_source + '.')
             else:
-                errorMessage('&read: File \'' + read_source + '\' does not exist.')
+                errorMessage(cmd + ': File \'' + read_source + '\' does not exist.')
             max_read_depth_ = max_read_depth_ + 1
         else:
-            errorMessage('&read: Nested level too deep; will not read ' + read_source + '.')
+            errorMessage(cmd + ': Nested level too deep; will not read ' + read_source + '.')
+
+    ####################
+
     elif cmd == 'set':
         parts = argtrim.split(' ')
         if (parts[0] == 'catfield'):
@@ -194,16 +296,66 @@ def parseDirective(line):
         elif (parts[0] == 'incfield'):
             incfield_ = int(parts[1])
             infoMessage('Setting increment field to ' + str(incfield_) + '.')
+
+    ####################
+
     elif cmd == 'stop':
         running_ = False
+
+    ####################
+
+    elif cmd == 'test':
+        if argtrim == None:
+            test(cmd + ': parameters required.')
+        elif argtrim.startswith('start'):
+            if not testing_:
+                m = re.search('^start\s+(\S*)$', argtrim)
+                if m:
+                    test_filename_ = m.group(1)
+                    testing_ = True
+                    test_pause_ = False
+                    testMessage('Test started with \'' + test_filename_ + '\'')
+                else:
+                    testMessage('Test filename not specified.')
+            else:
+                testMessage('Test is already running (' + test_filename_ + ').')
+        elif argtrim == 'pause':
+            if testing_:
+                test_pause_ = True
+                testMessage('Test paused.')
+            else:
+                testMessage('No test is currently running.')
+        elif argtrim == 'resume':
+            if testing_:
+                test_pause_ = False
+                testMessage('Test resumed.')
+            else:
+                testMessage('No test is currently running.')
+        elif argtrim == 'verbose':
+            test_verbose_ = True
+            testMessage('Test mode set to verbose.')
+        elif argtrim == 'quiet':
+            test_verbose_ = False
+            testMessage('Test mode set to quiet.')
+        elif argtrim == 'stop':
+            testStop()
+        else:
+            test(cmd + ': invalid parameter.')
+
+    ####################
+
     elif cmd == 'infomsg' and arg == 'on':
         infomsg_ = True
         infoMessage('Infomsg mode is on.')
     elif cmd == 'infomsg' and arg == 'off':
         infomsg_ = False
+
+    ####################
+
     else:
-        m = re.search('^(.*)\n$', line)
-        errorMessage('Invalid directive: ' + m.group(1))
+        errorMessage('Invalid directive: ' + line)
+
+################################################################################
 
 def parseLine(line):
     global fulbal_, clrbal_, catfield_, clrfield_, incfield_, decfield_
@@ -249,7 +401,7 @@ def parseLine(line):
         except ValueError:
             pass
         if output_:
-            print('{0} {1:8.2f} ({2:8.2f})'.format(out, fulbal_, clrbal_))
+            printLine('{0} {1:8.2f} ({2:8.2f})'.format(out, fulbal_, clrbal_))
 
 ####################
 # start here
@@ -258,14 +410,25 @@ def parseLine(line):
 # show information when starting in interactive mode
 show_info()
 
-# main parsing loop
 try:
+
+    # main parsing loop
     for line in fileinput.input():
+        line = re.sub('\n', '', line)
+        if not skipLine(line): parseLine(line)
         if not running_: break
-        if not skipLine(line): parseLine(re.sub('\n', '', line))
+
 except FileNotFoundError:
     errorMessage('Input file not found.')
+except ValueError:
+    errorMessage('Invalid input: ' + line)
+except:
+    errorMessage('Unexpected error: ' + line)
+    traceback.print_exc()
 finally:
     fileinput.close()
+    if testing_:
+        testStop()
+
 if goto_:
     errorMessage('EOF reached before tag \'' + goto_ + '\'.')
